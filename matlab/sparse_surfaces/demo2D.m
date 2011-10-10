9739edbd065ded9b0f0e71630b4aae7b890a0228
pkarasev <pkarasev@gatech.edu>
2011-10-10 19:20:53 -0400

    checkpoint 2D surface script

--- matlab/sparse_surfaces/demo2D.m
+++ matlab/sparse_surfaces/demo2D.m
@@ -1,4 +1,4 @@
-L=[0.2,0;0,0.2]
-mu=[0.01,0.03]
-k=1,b=0
-pts=gen2Dsparse(k,b,mu',L)
+
+[pts Nlines]=gen2D( );
+plot( pts(1,:), pts(2,:), 'rx' );
+title( ['Number of lines = ' num2str(Nlines) ]);
--- matlab/sparse_surfaces/gen2D.m
+++ matlab/sparse_surfaces/gen2D.m
@@ -1,28 +1,42 @@
-function pts = gen2D( Npoints, Nlines)
+function [pts Nlines] = gen2D( Npoints, Nlines,sigma)
 
-if( nargin ~= 2 )
+if( nargin ~= 3 )
   Npoints = 1000;
-  Nlines  = 5;
+  Nlines  = 2;
+  sigma   = 0.02;
 end
 
 % random number of lines
-Nlines   = poissrnd( Nlines );
+Nlines   = 3 + poissrnd( Nlines );
+Nlines   = min([ 8, Nlines]);
+
 
 % random number of points
-Npoints  = Npoints + randn(1,1) * Npoints * 0.1;
+Npoints  = round( Npoints + randn(1,1) * Npoints * 0.1 );
 
 % point partition (todo- be random as well)
 pts_k = round( linspace( 1, Npoints, Nlines ) );
 
 pts = zeros( 2, Npoints);
 
-for k = 1:Nlines
+for k = 1:(Nlines-1)
   
+  pt_a = [0;0]; pt_b = [0;0];
+  while( norm( pt_a - pt_b ) < 1/sqrt(2) )
+    pt_a = randn(2,1) + [k;0] - [Nlines/2;0];
+    pt_b = pt_a + randn(2,1) + [k;0] - [Nlines/2;0];
+  end
   
+  tt   = linspace(0,1,pts_k(k+1)-pts_k(k)+1);
+  pts(:,pts_k(k):pts_k(k+1)) = repmat( (pt_b ),1,numel(tt)) .* repmat( tt, 2, 1) + ...
+                               repmat( (pt_a),1,numel(tt)) .* repmat( 1-tt, 2, 1);
+                             
+  pts(:,pts_k(k):pts_k(k+1)) = pts(:,pts_k(k):pts_k(k+1)) + randn(size(pts(:,pts_k(k):pts_k(k+1))))*sigma;
   
 end
 
-
+Nlines = Nlines-1; % correction
+fprintf('');
 
 
 end
