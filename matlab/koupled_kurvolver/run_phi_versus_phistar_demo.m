4d25c317e3e2434e9f824e7758d5fdb4c1d94397
peter karasev <karasevpa@gmail.com>
2011-12-21 12:53:48 -0500

    update phi vs phistar demo

--- matlab/koupled_kurvolver/run_phi_versus_phistar_demo.m
+++ matlab/koupled_kurvolver/run_phi_versus_phistar_demo.m
@@ -1,4 +1,61 @@
-function [phi1 phi2 img_show U U0 tt xx yy] = run_phi_versus_phistar_demo()
+function run_phi_versus_phistar_demo()
+  set(0,'defaultaxesfontsize',16);  
+  set(0,'defaulttextfontsize',18);
+  set(0,'defaulttextfontname','Arial');
+  set(0,'defaultaxesfontweight','bold');
+  set(0,'defaultlinelinewidth',2);
+  set(0,'defaultlinemarkersize',4);
+  load_and_plot_multi_rho( )
+  dt_init         = 0.7; 
+  
+%   [Dval_alla t_alla] = run_core( sqrt(1/(2)) , dt_init);
+%   sfigure(2); semilogy(t_alla,Dval_alla,'--','color',[0 0 0.8]); hold on;  
+%   
+%   [Dval_allb t_allb] = run_core(     (1/(2)) ,dt_init);
+%    sfigure(2); semilogy(t_allb,Dval_allb,'-.','color',[0 0.4 .6]); hold on;  
+%    
+%   [Dval_allc t_allc] = run_core(     (1/(4)) ,dt_init);
+%    sfigure(2); semilogy(t_allc,Dval_allc,'--','color',[0 0.8 .2]); hold on;  
+%   
+   [Dval_alld t_alld] = run_core(     (1/(16)) ,dt_init);
+   sfigure(2); semilogy(t_alld,Dval_alld,'-.','color',[0.6 0.2 .2]); hold on;  
+   
+  [Dval_alle t_alle] = run_core(     (1/(256)) ,dt_init);
+   sfigure(2); semilogy(t_alle,Dval_alle,'--','color',[0.9 0.4 .2]); 
+   
+   
+   hold off;  
+  
+end
+
+function load_and_plot_multi_rho( )
+  files = {'bridge_demo_rho=0.0039062_19Dec2011-02-51.mat',...
+  'bridge_demo_rho=0.0625_19Dec2011-02-40.mat',...
+  'bridge_demo_rho=0.25_19Dec2011-02-22.mat',...
+  'bridge_demo_rho=0.5_19Dec2011-02-11.mat',...
+  'bridge_demo_rho=0.70711_19Dec2011-02-00.mat'};
+  sfigure(2); clf;  xlabel('time (sec)'); ylabel('labeling error');
+  N=numel(files);
+  for k = 1:N
+    data = load(files{k});
+    if(mod(k,2)==0)
+      sym='-.';
+    else
+      sym='--';
+    end
+    if(mod(k,3)==0)
+      sym='-';
+    end
+    semilogy(data.t_all,data.Dval_all,sym,'color',[(0.2+0.8*k/N) (0.4*(-1)^k+0.5)  1-k/N]); hold on;
+    fprintf('');
+  end
+  legend('\rho=1/64','\rho=1/16','\rho=1/4','\rho=1/2','\rho=1/2^{1/2}'); grid on;
+  axis([0 0.2 10^(-7) 10^4 ] ); hold off;
+  drawnow();
+  fprintf('');
+end
+
+function [Dval_all t_all phi1 phi2 img_show U tt xx yy] = run_core( rho_argin, dt_init )
 % run demo func in-place:
 % [phi1 phi2 img_show] = run_lskk_demo();
 
@@ -15,8 +72,7 @@ star_i    = star_i(keep_idx);
 star_j    = star_j(keep_idx);
 idx_left  = sub2ind( size(img), star_i, star_j);
 left_stub = img*0-1; left_stub( idx_left ) = 1;
-phi_star  = 3*tanh( imfilter( left_stub, fspecial('gaussian',[3 3],1.5),'replicate') );
-phi_star  = reinit_SD(phi_star, 1, 1, 0.5, 'ENO2', 100);
+
 
 img(145:156,120:145) =  img(150,124); % Make a 'bridge' connecting the two chunks
 
@@ -32,16 +88,28 @@ xy2     = [100,120]; xy1          = [120,92];
 RadInit = 15;
 d1      = RadInit - sqrt( ((xx-xy1(1))).^2 + ((yy-xy1(2))).^2 );
 d2      = RadInit - sqrt( ((xx-xy2(1))).^2 + ((yy-xy2(2))).^2 );
-phi1    = phi_star;
-phi2    = tanh(3*d2);
-phi2    = reinit_SD(phi2,1,1,0.5,'ENO2',20);
+
+if( exist('initial_data_phi_phi-star_img.mat','file' ) ) 
+  load initial_data_phi_phi-star_img.mat
+  assert( numel(phi2)==numel(img) ); assert( numel(phi_star)==numel(img) ); %#ok<NODEF>
+else
+  phi_star  = 3*tanh( imfilter( left_stub, fspecial('gaussian',[3 3],1.5),'replicate') );
+  phi_star  = reinit_SD(phi_star, 1, 1, 0.5, 'ENO3', 300);
+  phi2    = tanh(3*d2);
+  phi2    = reinit_SD(phi2,1,1,0.5,'ENO3',300);
+end
+
+phi1 = phi_star;
+
+if( ~exist('initial_data_phi_phi-star_img.mat','file' ) )
+  save('initial_data_phi_phi-star_img.mat','phi_star','phi2','img');
+end
+
 
 sfigure(1); clf;
 
-epsilon   = 0.8;
-zmax      = 20.0;
+epsilon   = sqrt(2); %1.1;%0.8;
 Heavi     = @(z)  1 * (z >= epsilon) + (abs(z) < epsilon).*(1+z/epsilon+1/pi * sin(pi*z/epsilon))/2.0;
-Heavi2    = @(z)  1 * (z < zmax) .* Heavi(z);
 delta     = @(z)  1 * (z == 0) + (abs(z) < epsilon).*(1 + cos(pi*z/epsilon))/(epsilon*2.0);
 
 % the cost of overlap that we want to shrink
@@ -76,28 +144,34 @@ delta_abs2 = [1];
 t_all      = [0];
 relTol     = 1e-4;
 absTol     = 1e2;
-phi_show_thresh = 0.9;
+phi_show_thresh = 0.95;
 tsum            = 0;
 U               = 0 * phi1;
 eps_u           = 1e-1;
 steps           = 0;
-MaxSteps        = 400;
+MaxSteps        = 500;
 Dval            = eval_label_dist(phi1,phi2);
 Dval_all        = [Dval];
 
 Gmax            = (max(img(:))-min(img(:))).^2; % maximum the G(\phi,I) term can ever be
-dt_init         = 0.7;
+
 dt0             = dt_init;
+MaxTime         = 0.25;
+dt_min          = 1e-3;
 
+rho                  =  rho_argin; %(1/2); % 1/16 %(1/4);
+U               =  sqrt(Gmax)/sqrt(rho) + 0*img;
 
-while( (tt < 3) && (steps < MaxSteps) )
+while( (tt < MaxTime) && (steps < MaxSteps) )
   
   prev_phi1            = phi1;
   g_gain               = 1e1;
   [phi2_pred ta gval]  = update_phi( img, phi2, 0 * phi2,0);
   phi1                 = phi_star;
-  U                    = max( abs(gval) .* g_gain, U );
-  U                    =  2 * sqrt(Gmax) + 0*img;
+  
+  assert( sum( U(:) < sqrt( abs(gval(:))/(rho) ) ) == 0 );
+  
+  
   eta                  = (Heavi(phi1)-Heavi(phi2));
   f_of_U               = -(U.^2).*(eta);
   
@@ -120,15 +194,14 @@ while( (tt < 3) && (steps < MaxSteps) )
   elseif( Dval_pred < Dval )
     fprintf('Warning, Dval rate worse with f(U), predicted value %f !? ',Dval_pred);
   end
-  if( bBadDval ) % If D failed to shrink, reduce the time step
-    dt0 = dt0 * 0.5;
-    phi_star  = reinit_SD(phi_star, 1, 1, dt0, 'ENO2', 3);
-    phi1      = phi_star;
-    phi2      = reinit_SD(phi2,     1, 1, dt0, 'ENO2', 3);
+  if( bBadDval ) % If D failed to shrink, get the previous phi, redistance a bit, reduce the time step
+    dt0 = max([dt_min, dt0 - 2e-1*dt0]);
+    phi2      = prev_phi2;
+    phi2      = reinit_SD(phi2,     1, 1, 0.25, 'ENO3', 2);
     fprintf(', dt0 = %f \n', dt0 );
     fprintf('');
   else           % Raise time-step back up if we're shrinking
-    dt0 = dt0 + 0.25 * ( dt_init - dt0 );
+    dt0 = min([dt0+1.3e-1*dt0, dt_init]);
   end
   
   tt         = tt + ta + tb;
@@ -156,8 +229,10 @@ fprintf('result = %f \n',result);
 
   function res = save_all( )
     fprintf('done! saving .... \n');
-    save run_openloop_bridge_demo t_all delta_abs1 delta_abs2 delta_rel1 delta_rel2 ... 
+    save run_openloop_bridge_demo t_all delta_abs1 delta_abs2 delta_rel1 delta_rel2 rho_argin... 
                              phi2_init phi2_mid img_show_mid img_show_init phi1 phi2 img img_show U tt xx yy  steps Dval_all 
+    setenv('rhoval',num2str(rho_argin))
+    !cp -v run_openloop_bridge_demo.mat  "bridge_demo_rho=${rhoval}_`date +%d%b%Y-%H-%M`.mat"
     res = 1;                               
   end
   function [Dval] = eval_label_dist( phiA, phiB )                             
@@ -190,7 +265,7 @@ fprintf('result = %f \n',result);
     dt_a  = dt0 / max(abs(dphi(:)));  % can go above 1 but then rel-step gets jagged...
     phi   = phi + dt_a * dphi;
     if( redist_iters > 0 )
-      phi =  reinit_SD(phi, 1, 1, dt0, 'ENO2', redist_iters);
+      phi =  reinit_SD(phi, 1, 1, dt0, 'ENO3', redist_iters);
     end
     
     
@@ -211,11 +286,11 @@ fprintf('result = %f \n',result);
     
     imgr( abs( phi1 ) < phi_show_thresh) = (imgr( abs( phi1 ) < phi_show_thresh) .* ... 
       abs( phi1(abs(phi1) < phi_show_thresh ) )/phi_show_thresh  + ...
-      1.5 * (phi_show_thresh - abs( phi1(abs(phi1) < phi_show_thresh ) ) )/phi_show_thresh );
+      1 * (phi_show_thresh - abs( phi1(abs(phi1) < phi_show_thresh ) ) )/phi_show_thresh );
     
     imgg( abs( phi2 ) < phi_show_thresh) = (imgg( abs( phi2 ) < phi_show_thresh) .* ... 
       abs( phi2(abs(phi2) < phi_show_thresh ) )/phi_show_thresh  + ...
-      1.5 * (phi_show_thresh - abs( phi2(abs(phi2) < phi_show_thresh ) ) )/phi_show_thresh );
+      1 * (phi_show_thresh - abs( phi2(abs(phi2) < phi_show_thresh ) ) )/phi_show_thresh );
     
    
     %imgb( abs(C21)>0 ) = (imgb(abs(C21)>0)/2 + abs(C21(abs(C21)>0))/max(abs(C21(:))) );
@@ -224,7 +299,7 @@ fprintf('result = %f \n',result);
     img_show(:,:,1) = imgr; img_show(:,:,2) = imgg; img_show(:,:,3) = imgb;
     img_show(img_show>1)=1; img_show(img_show<0)=0;
     sh=sfigure(1); subplot(1,2,2); imshow(img_show);
-    title(['image with coupled contours, ||U||_2=' num2str(norm(U)) ', t=' num2str(tt) ]);
+    title(['image and contours, ||U||_2=' num2str(norm(U)) ', t=' num2str(tt), ' steps = ' num2str_fixed_width(steps) ]);
     setFigure(sh,[10 10],3.5,1.9);
     fprintf( 'max-abs-phi = %f, t= %f, steps = %d \n',max(abs(phi1(:))),tt, steps);
     
@@ -234,8 +309,8 @@ fprintf('result = %f \n',result);
     sfigure(1); subplot(1,2,1);
     semilogy( t_all,Dval_all,'r-.' ); hold on;
     hold off;
-    legend('D(\phi,\phi^*');
-    title('relative levelset function change');
+    legend('D(\phi,\phi^*)'); xlabel('time (sec)');
+    title('labeling error');
     
     
     if( steps == 10 )
