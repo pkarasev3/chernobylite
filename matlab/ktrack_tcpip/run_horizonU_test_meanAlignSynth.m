0c86cbedee24364baef502a81585c0b2fe411284
pkarasev <pkarasev@gatech.edu>
2012-10-22 15:58:23 -0400

    touch

--- matlab/ktrack_tcpip/run_horizonU_test_meanAlignSynth.m
+++ matlab/ktrack_tcpip/run_horizonU_test_meanAlignSynth.m
@@ -4,7 +4,7 @@ function [phi1 phi2 img_show U U0 tt xx yy] = run_horizonU_test_meanAlignSynth()
 
 addpath('~/source/chernobylite/matlab/util/');
 addpath('~/source/chernobylite/matlab/display_helpers/');
-addpath('~/source/chernobylite/matlab/LevelSetMethods/');
+addpath('~/source/chernobylite/matlab/LSMlibPK/');
 
 m       = 256;
 n       = 256;
@@ -12,9 +12,12 @@ n       = 256;
 
 img1     = ((0.3 - sqrt( xx.^2 + yy.^2 )) > 0 ) * 1.0;
 img2     = 1.0*( yy < -0.2 ) .*(1+ (1-yy.^2).*(img1 < 0.5)*1.0);
+U        = imfilter( (-1 + 2*(img2>0)), fspecial('gaussian',[5 5],3),'replicate');
+
+CONTROL_IS_ON = false();
 
 img      = 0.5 * (img1 + img2);
-img      = img - min(img(:)); img = img / max(img(:));
+img      = img - min(img(:)); img = 10 * img / max(img(:));
 
 
 
@@ -25,12 +28,16 @@ xy2     = [0.15,0];
 RadInit = 0.2;
 d1      = RadInit - sqrt( ((xx-xy1(1))).^2 + ((yy-xy1(2))).^2 );
 d2      = RadInit - sqrt( ((xx-xy2(1))).^2 + ((yy-xy2(2))).^2 );
-phi1    = tanh(1e2*d1);
-phi2    = tanh(1e2*d2);
+phi1    = tanh(1e2*d1).*(xx.^2+yy.^2);
+phi2    = tanh(1e2*d2).*(xx.^2+yy.^2);
+
+phi2    = reinitializeLevelSetFunction(phi2, 2, 1.0, 100, 3, 2,true());
+phi2( max(abs(xx),abs(yy))>0.9 ) = min(phi2(:));
 
 sfigure(1); clf;
 
-epsilon   = 0.8;
+epsilon   = sqrt(2);
+dX        = 1.0;
 Heavi     = @(z)  1 * (z >= epsilon) + (abs(z) < epsilon).*(1+z/epsilon+1/pi * sin(pi*z/epsilon))/2.0;
 delta     = @(z)  (abs(z) < epsilon).*(1 + cos(pi*z/epsilon))/(epsilon*2.0);
 
@@ -47,7 +54,7 @@ sfigure(1); subplot(2,1,2);  plot(x,Heavi(x),'r--'); hold on; plot(x,delta(x),'b
 
 
 tt = 0;
-img0 = img;
+img0 = img / max(img(:));
 %img  = imfilter( img * 255, fspecial('gaussian',[5 5],1),'replicate');
 %img  = img - mean(img(:));
 
@@ -65,49 +72,17 @@ mu2_in_out = [mu_i2;mu_o2];
 t_all      = [0];
 relTol     = 1e-4;
 absTol     = 1e2;
-phi_show_thresh = 0.9;
+phi_show_thresh = epsilon;
 tsum            = 0;
-U               = 0 * phi1;
+
 eps_u           = 1e-1;
 steps           = 0;
-MaxSteps        = 1000;
+MaxSteps        = 200;
 img_pt          = img( size(img,1)/2, size(img,1)/2 + 5 );
-while( ( (min([delta_rel1(end),delta_rel2(end)]) > relTol)  || ...
-    (min([delta_abs1(end),delta_abs2(end)]) > absTol) ) &&  (steps < MaxSteps) )
-  steps = steps + 1 + eps_u - eps_u;
+while( (steps < MaxSteps) )
+
+  steps = steps + 1 ;
   
-  % Create instantaneous state change every so often
-  if( tsum > 1 ) 
-    tsum           = 0;
-    Uxy1 = RadInit*[cos(pi/3);  sin(pi/3)];  % Input U(x,y,t)
-    Uxy2 = RadInit*[cos(-pi/3); sin(-pi/3)]; % Input U(x,y,t)
-    Uxy3 = RadInit*[cos(0); sin(0)]; % Input U(x,y,t)
-    U0   = 1;
-    Rin  = 0.1;
-    dU1   = U0*Heavi( (Rin - sqrt( ((xx-Uxy1(1))).^2 + ((yy-Uxy1(2))).^2 ) )*1e1 );
-    dU2   = U0*Heavi( (Rin - sqrt( ((xx-Uxy2(1))).^2 + ((yy-Uxy2(2))).^2 ) )*1e1 );
-    dU3   = U0*Heavi( (Rin - sqrt( ((xx-Uxy3(1))).^2 + ((yy-Uxy3(2))).^2 ) )*1e1 );
-    dU1   = dU1 .* ( abs(img - img_pt)<1e-3 );
-    dU2   = dU2 .* ( abs(img - img_pt)<1e-3 );
-    dU3   = dU3 .* ( abs(img - img_pt)<1e-3 );
-    dU    = dU1+dU2+dU3;
-    U    = U + dU - (eps_u * U).^3;
-%     phi2( 0 < (dU < 0).*(phi2>0)  ) = -1;
-%     phi2( 0 < (dU > 0).*(phi2<=0) ) = +1;
-%     phi2 =  reinit_SD(phi2, 1, 1, 0.8, 'ENO2', 10);
-    
-    fprintf('added input at time %f, max U = %f, norm U = %f \n', ...
-      tt, max(abs(U(:))), norm(U(:)) );
-    
-    fprintf('');
-    
-  end
-  
-  
-%   CouplingSymmetric = 0*(Heavi(phi1*1e2).*Heavi(phi2*1e2));
-%   C12        = CouplingSymmetric;
-%   prev_phi1  = phi1;
-%   [phi1 ta mui1 muo1]  = update_phi( img, phi1, C12);
   ta = 0; mui1=0; muo1=0; prev_phi1 = phi2;
   CouplingSymmetric =0*(Heavi(phi1*1e2).*Heavi(phi2*1e2));
   C21        = CouplingSymmetric  ;%(U.^2).*(-Heavi(U)+Heavi(phi2));
@@ -151,11 +126,22 @@ fprintf('done! saving (?).... \n');
     
     [mu_i, mu_o] = compute_means(Img,phi);
     
+    [U_i, U_o] = compute_means(U,phi);
     
     kappa_phi(1:numel(phi)) = kappa(phi,1:numel(phi));
     
-    g_alpha= (Img - mu_i).^2 - (Img - mu_o).^2 + Coupling;
-    dphi  = delta(phi) .* (-g_alpha + lambda * kappa_phi) ;
+    g_alpha= -(Img - mu_i).^2 + (Img - mu_o).^2;
+    
+    % f: |U-U_i| < uMin  maps to 0
+    % f: |U-U_i| >=1  maps to -G 
+    uMin   =        0.1;
+    xi_sqr = (U-U_i).^2; 
+    f_of_U = (xi_sqr > 0.1).*( min(xi_sqr - uMin, 1 ) ).*(-g_alpha);
+    if ~CONTROL_IS_ON
+      f_of_U = 0*f_of_U;
+    end
+    
+    dphi  = delta(phi) .* ( g_alpha + f_of_U + lambda * kappa_phi) ;
     
     fprintf('mu_i = %f, mu_o = %f, g_alpha max = %f, lam*kap max = %f,',...
       mu_i,mu_o,max(abs(g_alpha(:))),max(abs(lambda*kappa_phi(:))));
@@ -163,7 +149,10 @@ fprintf('done! saving (?).... \n');
     dt0   = 0.8;
     dt_a  = dt0 / max(abs(dphi(:)));  % can go above 1 but then rel-step gets jagged...
     phi   = phi + dt_a * dphi;
-    phi =  reinit_SD(phi, 1, 1, dt0, 'ENO2', 2);
+    
+    
+    phi    = reinitializeLevelSetFunction(phi, 2, dX, 2, 3, 2, true() );
+    %phi =  reinit_SD(phi, 1, 1, dt0, 'ENO2', 2);
     
     
     
@@ -177,14 +166,14 @@ fprintf('done! saving (?).... \n');
     
     % zero out the non-active colors for phi1 (active red), phi2 (active green)
     imgr( abs( phi2 ) < phi_show_thresh ) = 0;
-    imgg( abs( phi1 ) < phi_show_thresh ) = 0;
+%     imgg( abs( phi1 ) < phi_show_thresh ) = 0;
     imgb( abs( phi2 ) < phi_show_thresh ) = 0;
-    imgb( abs( phi1 ) < phi_show_thresh ) = 0;
+%     imgb( abs( phi1 ) < phi_show_thresh ) = 0;
     
-    imgr( abs( phi1 ) < phi_show_thresh) = (imgr( abs( phi1 ) < phi_show_thresh) .* ...
-      abs( phi1(abs(phi1) < phi_show_thresh ) )/phi_show_thresh  + ...
-      1.5 * (phi_show_thresh - abs( phi1(abs(phi1) < phi_show_thresh ) ) )/phi_show_thresh );
-    
+%     imgr( abs( phi1 ) < phi_show_thresh) = (imgr( abs( phi1 ) < phi_show_thresh) .* ...
+%       abs( phi1(abs(phi1) < phi_show_thresh ) )/phi_show_thresh  + ...
+%       1.5 * (phi_show_thresh - abs( phi1(abs(phi1) < phi_show_thresh ) ) )/phi_show_thresh );
+%     
     imgg( abs( phi2 ) < phi_show_thresh) = (imgg( abs( phi2 ) < phi_show_thresh) .* ...
       abs( phi2(abs(phi2) < phi_show_thresh ) )/phi_show_thresh  + ...
       1.5 * (phi_show_thresh - abs( phi2(abs(phi2) < phi_show_thresh ) ) )/phi_show_thresh );
@@ -195,26 +184,27 @@ fprintf('done! saving (?).... \n');
 %     
     img_show(:,:,1) = imgr; img_show(:,:,2) = imgg; img_show(:,:,3) = imgb;
     img_show(img_show>1)=1; img_show(img_show<0)=0;
-    sh=sfigure(1); subplot(1,2,2); imshow(img_show);
-    title(['image with coupled contours, ||U||_2=' num2str(norm(U)) ', t=' num2str(tt) ]);
-    setFigure(sh,[10 10],3.2,1.5);
+    sh=sfigure(1); clf; %subplot(1,2,2); 
+    imshow(img_show);
+    %title(['image with coupled contours, ||U||_2=' num2str(norm(U)) ', t=' num2str(tt) ]);
+    %setFigure(sh,[10 10],3.2,1.5);
     fprintf( 'max-abs-phi = %f, t= %f, steps = %d \n',max(abs(phi1(:))),tt, steps);
     
    % imwrite(img_show,['control_chanvese_demo_' num2str_fixed_width(steps) '.png']);
     
     
-    sfigure(1); subplot(1,2,1);
-    semilogy( t_all,delta_rel1,'r-.' ); hold on;
-    semilogy( t_all,delta_rel2,'g--');
-    semilogy( t_all,delta_abs1,'m-.' );
-    semilogy( t_all,delta_abs2,'c--');
-    hold off;
-    legend('\Delta-rel for \phi_1','\Delta-rel for \phi_2', ...
-      '\Delta-abs for \phi_1','\Delta-abs for \phi_2');
-    title('relative levelset function change');
-    
-    drawnow;
-    
+%     sfigure(1); subplot(1,2,1);
+%     semilogy( t_all,delta_rel1,'r-.' ); hold on;
+%     semilogy( t_all,delta_rel2,'g--');
+%     semilogy( t_all,delta_abs1,'m-.' );
+%     semilogy( t_all,delta_abs2,'c--');
+%     hold off;
+%     legend('\Delta-rel for \phi_1','\Delta-rel for \phi_2', ...
+%       '\Delta-abs for \phi_1','\Delta-abs for \phi_2');
+%     title('relative levelset function change');
+%     
+      drawnow;
+     
     
   end
 
